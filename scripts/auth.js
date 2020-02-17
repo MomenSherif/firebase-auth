d9eb890bbd4749cfa77cf692037d48bb3fb44d24
MomenSherif <momensherif.2019@gmail.com>
2020-02-17 23:24:02 +0200

    Fixed login bug, update menu UI depend on Auth state

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,10 +1,45 @@
+// track auth state
+auth.onAuthStateChanged(function(user) {
+  if (user) {
+    // update menu if logged in
+    document
+      .querySelectorAll('.logged-out')
+      .forEach(el => el.classList.add('hide'));
+
+    document
+      .querySelectorAll('.logged-in')
+      .forEach(el => el.classList.remove('hide'));
+
+    // get guides data from firestore
+    db.collection('guides')
+      .get()
+      // returns querySnapshot has docs array
+      .then(({ docs }) => {
+        setupGuides(docs.map(doc => doc.data()));
+      });
+  } else {
+    // update menu if not logged in
+    document
+      .querySelectorAll('.logged-in')
+      .forEach(el => el.classList.add('hide'));
+
+    document
+      .querySelectorAll('.logged-out')
+      .forEach(el => el.classList.remove('hide'));
+
+    // User signed out
+    setupGuides([]);
+  }
+});
+
+// signup new account
 const signUpForm = document.getElementById('signup-form');
 signUpForm.addEventListener('submit', e => {
   e.preventDefault();
   const email = signUpForm.email.value.trim();
   const password = signUpForm.password.value.trim();
 
-  // after sign up firebase login user automatically
+  // after signup firebase login user automatically
   auth
     .createUserWithEmailAndPassword(email, password)
     .then(cred => {
@@ -15,31 +50,23 @@ signUpForm.addEventListener('submit', e => {
     .catch(error => console.log(error));
 });
 
+// signin
 const loginForm = document.getElementById('login-form');
 loginForm.addEventListener('submit', e => {
   e.preventDefault();
-  const email = signUpForm.email.value.trim();
-  const password = signUpForm.password.value.trim();
+  const email = loginForm.email.value.trim();
+  const password = loginForm.password.value.trim();
+
   auth
     .signInWithEmailAndPassword(email, password)
     .then(cred => {
-      console.log(cred);
       M.Modal.getInstance(document.getElementById('modal-login')).close();
       loginForm.reset();
     })
     .catch(err => console.log(err));
 });
 
+// logout
 document.getElementById('logout').addEventListener('click', e => {
   auth.signOut();
-});
-
-auth.onAuthStateChanged(function(user) {
-  if (user) {
-    // User is signed in
-    console.log(user);
-  } else {
-    // User signed out
-    console.log('signed out');
-  }
 });
--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,20 @@
+const guidesList = document.querySelector('.guides');
+function setupGuides(guides) {
+  if (guides.length > 0) {
+    guidesList.innerHTML = guides
+      .map(({ title, description }) => {
+        return `
+      <li>
+      <div class="collapsible-header grey lighten-4">${title}</div>
+      <div class="collapsible-body white">${description}</div>
+      </li>`;
+      })
+      .join('');
+  } else {
+    guidesList.innerHTML = '<h4 class="center-align">Login for Guides</h4>';
+  }
+}
+
 // setup materialize components
 document.addEventListener('DOMContentLoaded', function() {
   const modals = document.querySelectorAll('.modal');
@@ -5,21 +22,4 @@ document.addEventListener('DOMContentLoaded', function() {
 
   const items = document.querySelectorAll('.collapsible');
   M.Collapsible.init(items);
-
-  // get guides from firebase and add them to list
-  const guides = document.querySelector('.guides');
-  db.collection('guides')
-    .get()
-    .then(querySnapshot => {
-      let html = '';
-      querySnapshot.forEach(doc => {
-        const { title, description } = doc.data();
-        html += `
-        <li>
-          <div class="collapsible-header grey lighten-4">${title}</div>
-          <div class="collapsible-body white">${description}</div>
-        </li>`;
-      });
-      guides.insertAdjacentHTML('beforeend', html);
-    });
 });
